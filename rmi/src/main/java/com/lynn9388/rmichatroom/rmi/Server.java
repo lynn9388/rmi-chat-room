2a01ab52ac8cd4332b08b3bd79fa204963ca787e
Lynn <lynn9388@gmail.com>
2016-10-24 21:56:04 +0800

    Add parameters for Server RMI

--- rmi/src/main/java/com/lynn9388/rmichatroom/rmi/Server.java
+++ rmi/src/main/java/com/lynn9388/rmichatroom/rmi/Server.java
@@ -18,14 +18,15 @@ package com.lynn9388.rmichatroom.rmi;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.Date;
 import java.util.List;
 
 public interface Server extends Remote {
     String NAME = "Server";
 
-    boolean register(String username) throws RemoteException;
+    boolean register(User user) throws RemoteException;
 
     List<User> getUsers() throws RemoteException;
 
-    void recordMessage(String message) throws RemoteException;
+    void recordMessage(String from, String to, Date date, String message) throws RemoteException;
 }
